d7c62ec2aef2c79a320dfba298a95b1330314c37
Sebastian Garrido <sebasgarcep@gmail.com>
2017-08-07 11:41:00 -0500

    feat: working stream with reasonable speed

--- adaboost-stump/src/main.rs
+++ adaboost-stump/src/main.rs
@@ -1,108 +1,70 @@
-/**
-*
-*   Dependencies:
-*
-*   gdk-pixbuf = "0.1.3"
-*   glib = "0.1.3"
-*   gtk = "0.1.3"
-*   rscam = "0.5.3"
-*
-*/
+// FIND v4l2 COMPATIBLE SPECS: v4l2-ctl --list-formats-ext
+// Thanks to https://github.com/oli-obk/camera_capture for most of the code
 
+extern crate piston_window;
+extern crate image;
 extern crate rscam;
-extern crate gtk;
-extern crate gdk_pixbuf;
-extern crate glib;
 
-use std::fs;
-use std::io::Write;
 use std::thread::sleep;
-use std::cell::RefCell;
-use std::sync::mpsc::{channel, Receiver};
 use std::time::Duration;
-use std::thread;
-use rscam::{Camera, Config};
-use gtk::prelude::*;
-use gtk::{Button, Image, Window, WindowType};
-use gdk_pixbuf::Pixbuf;
-use glib::{Continue, timeout_add};
-
-fn get_pixbuf_from_camera(camera: &Camera) -> Pixbuf {
-    let frame = camera.capture().unwrap();
-    let (pic_width, pic_height) = frame.resolution;
-
-    let colorspace = 0;
-    let rowstride = 3 * pic_width;
-    let vec = Vec::from(&frame[..]);
-
-    Pixbuf::new_from_vec(vec,
-                         colorspace,
-                         false,
-                         8,
-                         pic_width as i32,
-                         pic_height as i32,
-                         rowstride as i32)
-}
-
-fn receive() -> glib::Continue {
-    GLOBAL.with(|global| {
-        if let Some((ref camera, ref img)) = *global.borrow() {
-            let pixbuf = get_pixbuf_from_camera(&camera);
-            img.set_from_pixbuf(Some(&pixbuf));
-        }
-    });
-    glib::Continue(false)
-}
+use piston_window::{PistonWindow, Texture, WindowSettings, TextureSettings, clear};
+use image::load_from_memory;
+use image::{ConvertBuffer, DynamicImage};
 
 fn main() {
-    const WIDTH: u32 = 800;
-    const HEIGHT: u32 = 600;
+    const WIDTH: u32 = 640;
+    const HEIGHT: u32 = 480;
 
-    if gtk::init().is_err() {
-        println!("Failed to initialize GTK.");
-        return;
-    }
-
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("First GTK+ Program");
-    window.set_default_size(WIDTH as i32, HEIGHT as i32);
-
-    let mut camera = rscam::new("/dev/video0").unwrap();
-    camera.start(&rscam::Config {
-            interval: (1, 30), // 30 fps.
-            resolution: (1280, 720),
-            format: b"YUYV",
-            ..Default::default()
-        })
+    let mut window: PistonWindow = WindowSettings::new("piston: image", [WIDTH, HEIGHT])
+        .exit_on_esc(true)
+        .build()
         .unwrap();
 
-    let pixbuf = get_pixbuf_from_camera(&camera);
-    let img = Image::new_from_pixbuf(Some(&pixbuf));
-    window.add(&img);
+    let mut tex: Option<Texture<_>> = None;
+    let (sender, receiver) = std::sync::mpsc::channel();
+    let imgthread = std::thread::spawn(move || {
+        let mut camera = rscam::new("/dev/video0").unwrap();
+        camera.start(&rscam::Config {
+                interval: (1, 30), // 30 fps.
+                format: b"MJPG",
+                resolution: (WIDTH, HEIGHT),
+                ..Default::default()
+            })
+            .unwrap();
 
-    // put TextBuffer and receiver in thread local storage
-    GLOBAL.with(move |global| *global.borrow_mut() = Some((camera, img)));
-
-    thread::spawn(move || {
         loop {
-            // do long work
-            thread::sleep(Duration::from_millis(50));
-            // receive will be run on the main thread
-            glib::idle_add(receive);
+            let frame = camera.capture().unwrap();
+            let img = match load_from_memory(&frame).unwrap() {
+                    DynamicImage::ImageRgb8(rgb_image) => rgb_image,
+                    _ => panic!(),
+                }
+                .convert();
+
+            if let Err(_) = sender.send(img) {
+                break;
+            }
+
+            sleep(Duration::from_millis(50));
         }
     });
 
-    window.show_all();
+    while let Some(e) = window.next() {
+        if let Ok(img) = receiver.try_recv() {
+            if let Some(mut t) = tex {
+                t.update(&mut window.encoder, &img).unwrap();
+                tex = Some(t);
+            } else {
+                tex = Texture::from_image(&mut window.factory, &img, &TextureSettings::new()).ok();
+            }
+        }
+        window.draw_2d(&e, |c, g| {
+            clear([1.0; 4], g);
+            if let Some(ref t) = tex {
+                piston_window::image(t, c.transform, g);
+            }
+        });
+    }
 
-    window.connect_delete_event(|_, _| {
-        gtk::main_quit();
-        Inhibit(false)
-    });
-
-    gtk::main();
+    drop(receiver);
+    imgthread.join().unwrap();
 }
-
-// declare a new thread local storage key
-thread_local!(
-    static GLOBAL: RefCell<Option<(Camera, Image)>> = RefCell::new(None)
-);
