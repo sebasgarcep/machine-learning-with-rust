1e775ea459feef616a705ea0ace0a575b3fe8ab8
Sebastian Garrido <sebasgarcep@gmail.com>
2017-08-14 09:29:20 -0500

    finished

--- adaboost-stump/README.md
+++ adaboost-stump/README.md
@@ -1,5 +1,16 @@
 # AdaBoost-Decision Stump Viola Jones (2001)
 
-### To run:
+### To train the model:
 ```bash
 cargo run --bin train --release
+```
+
+### To see static results:
+```bash
+cargo run --bin validate --release
+```
+
+### TODO:
+- Real Time Face Detection
+  - Doing detection on every frame
+  - Doing some magic to scale sizes to ones the predictor understands (19x19).
--- adaboost-stump/src/validate.rs
+++ adaboost-stump/src/validate.rs
@@ -4,6 +4,7 @@ extern crate serde;
 extern crate serde_json;
 extern crate image;
 extern crate rulinalg;
+extern crate piston_window;
 
 mod shared;
 mod integral_image;
@@ -12,25 +13,31 @@ mod haar_like_feature;
 
 use std::fs::File;
 use std::io::Read;
-use image::{DynamicImage, Pixel};
+use image::{ConvertBuffer, DynamicImage, Pixel};
 use rulinalg::matrix::Matrix;
 use integral_image::IntegralImage;
 use shared::{WINDOW_HEIGHT, WINDOW_WIDTH};
 use prediction_ensemble::PredictionEnsemble;
+use piston_window::{PistonWindow, Texture, WindowSettings, TextureSettings};
+use piston_window::rectangle::Rectangle;
 
 fn main() {
-    let beatles = match image::open("./data/beatles.jpg").unwrap() {
+    let data = match image::open("./data/got.jpeg").unwrap() {
         DynamicImage::ImageLuma8(gray_image) => gray_image,
+        DynamicImage::ImageRgb8(rgb_image) => rgb_image.convert(),
+        DynamicImage::ImageRgba8(rgba_image) => rgba_image.convert(),
         _ => unreachable!(),
     };
 
-    let (width, height) = beatles.dimensions();
+    let (width, height) = data.dimensions();
 
-    let mut data = String::new();
+    let mut model_raw = String::new();
     let mut f = File::open("./foo.json").expect("Unable to open file");
-    f.read_to_string(&mut data).expect("Unable to read string");
+    f.read_to_string(&mut model_raw).expect("Unable to read string");
 
-    let ensemble: PredictionEnsemble = serde_json::from_str(&data).unwrap();
+    let ensemble: PredictionEnsemble = serde_json::from_str(&model_raw).unwrap();
+
+    let mut coll = Vec::new();
 
     for y in 0..((height as usize) - WINDOW_HEIGHT) {
         for x in 0..((width as usize) - WINDOW_WIDTH) {
@@ -39,7 +46,7 @@ fn main() {
 
             for i in 0..WINDOW_HEIGHT {
                 for j in 0..WINDOW_WIDTH {
-                    let pixel = beatles.get_pixel((x + i) as u32, (y + j) as u32);
+                    let pixel = data.get_pixel((x + i) as u32, (y + j) as u32);
                     let mat_index = [i as usize, j as usize];
                     let value = (pixel.channels()[0]) as f64;
                     mat[mat_index] = value;
@@ -55,8 +62,32 @@ fn main() {
             let integral_image = IntegralImage::build(&mat);
 
             if ensemble.predict(&integral_image) {
-                println!("x: {:?} y: {:?}", x, y);
+                coll.push((x, y, WINDOW_WIDTH, WINDOW_HEIGHT));
             }
         }
+    }
+
+    let mut window: PistonWindow = WindowSettings::new("piston: image", [width, height])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let img = data.convert();
+
+    let mut tex: Texture<_> =
+        Texture::from_image(&mut window.factory, &img, &TextureSettings::new()).ok().unwrap();
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            piston_window::clear([1.0; 4], g);
+
+            piston_window::image(&tex, c.transform, g);
+
+            for &(x, y, ww, wh) in coll.iter() {
+                let rect = Rectangle::new_border([0.0, 1.0, 0.0, 0.2], 1.0);
+                let pos = [x as f64, y as f64, ww as f64, wh as f64];
+                rect.draw(pos, &c.draw_state, c.transform, g)
+            }
+        });
     }
 }
